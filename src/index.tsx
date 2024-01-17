9bd5d6cf396d5cbb137a87d34a2b6e55389c08a0
wesleywil <wesleywilsonti@gmail.com>
2024-01-17 13:08:19 -0300

    updated index.tsx by adding the new Chat page

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Router, Route } from "@solidjs/router";
 
 import "./index.css";
 import App from "./App";
+import Chat from "./chat/Chat";
 
 const root = document.getElementById("root");
 
@@ -17,6 +18,7 @@ render(
   () => (
     <Router>
       <Route path="/" component={App} />
+      <Route path="/chat" component={Chat} />
     </Router>
   ),
   root!
