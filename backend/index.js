3d0fecc03e9a791c2afb4a24392a03ac0b7c206f
wesleywil <wesleywilsonti@gmail.com>
2024-01-20 13:17:47 -0300

    added a backend folder too handle all backend logic, added and configured the socket.io depedency with some basic logic for testing the connection with the frontend

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "node:http";
+import cors from "cors";
+import { Server as SocketIOServer } from 'socket.io';
+
+const app = express();
+const PORT = 5000;
+
+const server = http.Server(app);
+
+app.use(cors());
+
+const socketIO = new SocketIOServer(server, {
+    cors:{
+        origin:"http://localhost:3000"
+    }
+});
+
+socketIO.on('connection', (socket)=>{
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', ()=>{
+        console.log('🔥: A user disconnected');
+    })
+})
+
+app.get("/api", (req, res)=>{
+    res.json({
+        message:"Hello world!",
+    })
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT}`)
+})
+
+
+
