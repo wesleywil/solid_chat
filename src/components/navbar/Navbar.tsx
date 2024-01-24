6459fc6e9cc4d782ff46c2d611e72655ad1d8adf
wesleywil <wesleywilsonti@gmail.com>
2024-01-24 14:16:14 -0300

    updated Navbar component by including the signIn anchor allowing users to navigate to the signIn page and added a button to handle user disconnecting. Updated the Navbar css module by fixing some details like the width that could not be set to 100% percent without overriding with the borders

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,37 @@
 import { Component } from "solid-js";
+import { Socket } from "socket.io-client";
 
 import styles from "./Navbar.module.css";
 import logo from "../../assets/logo.webp";
 
-const Navbar: Component = () => {
+const Navbar: Component<{ socket: Socket }> = (props) => {
+  const handleDisconnect = () => {
+    localStorage.removeItem("username");
+    window.location.reload();
+  };
   return (
     <header class={styles.container}>
-      <a href="/" class={styles.navbar_logo}>
-        <img src={logo} alt="logo" class={styles.logo} />
-      </a>
-      <div class={styles.navbar_links}>
-        <a href="/chat" class={styles.navbar_link}>
-          Chat
+      <div class={styles.subcontainer}>
+        <a href="/" class={styles.navbar_logo}>
+          <img src={logo} alt="logo" class={styles.logo} />
         </a>
+        <div class={styles.navbar_links}>
+          <a href="/chat" class={styles.navbar_link}>
+            Chat
+          </a>
+          {localStorage.getItem("username")?.length ? (
+            <button
+              onClick={handleDisconnect}
+              class={styles.navbar_btn_disconnect}
+            >
+              Disconnect
+            </button>
+          ) : (
+            <a href="/signIn" class={styles.navbar_link}>
+              SignIn
+            </a>
+          )}
+        </div>
       </div>
     </header>
   );
