f2d7edf2fee81f334bb32b85a5db894512744f7d
wesleywil <wesleywilsonti@gmail.com>
2024-01-29 13:41:40 -0300

    updated ChatFriendList component and styles, by adding a function to close the friend list when the user click outside of the component

--- src/components/chat_friend_list/ChatFriendList.tsx
+++ src/components/chat_friend_list/ChatFriendList.tsx
@@ -1,14 +1,24 @@
-import { Component, createEffect, createSignal, For, JSX } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  For,
+  JSX,
+  onCleanup,
+} from "solid-js";
 import { Socket } from "socket.io-client";
+import { User } from "../../utils/interfaces";
+import useRedux from "../../redux/useRedux";
+import { setHideFriendList, utilStore } from "../../redux/utils/utils";
 
 import styles from "./ChatFriendList.module.css";
 import logo from "../../assets/logo.webp";
 
 // Components
 import ChatFriendListItem from "../chat_friend_list_item/ChatFriendListItem";
-import { User } from "../../utils/interfaces";
 
 const ChatFriendList: Component<{ socket: Socket }> = (props) => {
+  const [state, actions] = useRedux(utilStore, { setHideFriendList });
   const [allUsers, setAllUsers] = createSignal<User[]>([]);
   const [users, setUsers] = createSignal<User[]>([]);
   const [filter, setFilter] = createSignal("");
@@ -27,19 +37,35 @@ const ChatFriendList: Component<{ socket: Socket }> = (props) => {
     setUsers(filteredUsers);
   };
 
+  const handleClickOutside: JSX.EventHandler<HTMLDivElement, MouseEvent> = (
+    e
+  ) => {
+    const subContainer = document.querySelector(`.${styles.sub_container}`);
+
+    if (subContainer && !subContainer.contains(e.target)) {
+      if (!state.hideFriendList) {
+        actions.setHideFriendList();
+      }
+    }
+  };
+
   return (
-    <div class={styles.container}>
-      <img src={logo} alt="logo" class={styles.container_image} />
-      <h1 class={styles.title}>Friend List</h1>
-      <input
-        type="text"
-        placeholder="Search Contacts"
-        class={styles.search_input}
-        value={filter()}
-        onInput={(e) => handleFilter(e)}
-      />
-      <div class={styles.friend_list}>
-        <For each={users()}>{(item) => <ChatFriendListItem user={item} />}</For>
+    <div class={styles.container} onClick={handleClickOutside}>
+      <div class={styles.sub_container}>
+        <img src={logo} alt="logo" class={styles.container_image} />
+        <h1 class={styles.title}>Friend List</h1>
+        <input
+          type="text"
+          placeholder="Search Contacts"
+          class={styles.search_input}
+          value={filter()}
+          onInput={(e) => handleFilter(e)}
+        />
+        <div class={styles.friend_list}>
+          <For each={users()}>
+            {(item) => <ChatFriendListItem user={item} />}
+          </For>
+        </div>
       </div>
     </div>
   );
