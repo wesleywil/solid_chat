8c3f3a8753a9ebacfc2700859c3275d42ac797a0
wesleywil <wesleywilsonti@gmail.com>
2024-01-30 13:04:11 -0300

    updated Navbar by removing some logic, making unecessary the disconnect button and changing the signIn button name to 'new account'

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,10 @@
-import { Component, createEffect } from "solid-js";
+import { Component } from "solid-js";
 import { Socket } from "socket.io-client";
-import useRedux from "../../redux/useRedux";
 
 import styles from "./Navbar.module.css";
 import logo from "../../assets/logo.webp";
-import { setShowDisconnect, utilStore } from "../../redux/utils/utils";
 
 const Navbar: Component<{ socket: Socket }> = (props) => {
-  const [state, actions] = useRedux(utilStore, { setShowDisconnect });
-  const handleDisconnect = () => {
-    localStorage.removeItem("username");
-    actions.setShowDisconnect(false);
-  };
-
-  createEffect(() => {
-    //Upade logic here when "username" is removed from local storage
-    if (localStorage.getItem("username")) {
-      actions.setShowDisconnect(true);
-    }
-  });
   return (
     <header class={styles.container}>
       <div class={styles.subcontainer}>
@@ -29,18 +15,9 @@ const Navbar: Component<{ socket: Socket }> = (props) => {
           <a href="/chat" class={styles.navbar_link}>
             Chat
           </a>
-          {state.showDisconnect ? (
-            <button
-              onClick={handleDisconnect}
-              class={styles.navbar_btn_disconnect}
-            >
-              Disconnect
-            </button>
-          ) : (
-            <a href="/signIn" class={styles.navbar_link}>
-              SignIn
-            </a>
-          )}
+          <a href="/signIn" class={styles.navbar_link}>
+            New Account
+          </a>
         </div>
       </div>
     </header>
