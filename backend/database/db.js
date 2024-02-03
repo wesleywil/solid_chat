0b0b7cd22e818ffb8ca4dfa41f055fea5b356ffd
wesleywil <wesleywilsonti@gmail.com>
2024-02-03 14:05:09 -0300

    updated main database functions to utilize promises for improved asynchronous handling and better readability

--- backend/database/db.js
+++ backend/database/db.js
@@ -8,33 +8,48 @@ db.run(
   "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, socketID TEXT NOT NULL)"
 );
 
-export const getAllUsers = (callback) => {
-  const selectQuery = "SELECT * FROM users";
-
-  db.all(selectQuery, [], function (err, rows) {
-    callback(err, rows);
+export const getAllUsers = () => {
+  return new Promise((resolve, reject) => {
+    const selectQuery = "SELECT * FROM users";
+    db.all(selectQuery, [], function (err, rows) {
+      if (err) {
+        console.error("Error from database fetching users: ", err.message);
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
   });
 };
 
-export const createUser = (username, socketID, callback) => {
-  const insertQuery = `INSERT INTO users (username, socketID) VALUES (?,?)`;
-  const params = [username, socketID];
-  db.run(insertQuery, params, function (err) {
-    console.log("edsovjh ", insertQuery);
-    if (err) {
-      return callback(err, null);
-    }
-    console.log('USER CREATED')
-    callback(null, this.lastID);
+export const createUser = (username, socketID) => {
+  return new Promise((resolve, reject) => {
+    const insertQuery = `INSERT INTO users (username, socketID) VALUES (?,?)`;
+    const params = [username, socketID];
+    db.run(insertQuery, params, function (err) {
+      if (err) {
+        console.error("Error from database deleting user: ", err.message);
+        reject(err);
+      } else {
+        console.log("USER CREATED");
+        resolve(this.lastID);
+      }
+    });
   });
 };
 
-export const deleteUser = (socketID, callback) => {
-  const deleteQuery = "DELETE FROM users WHERE socketID = ?";
-  const params = [socketID];
-
-  db.run(deleteQuery, params, function (err) {
-    console.log('USER DELETED');
-    callback(err);
+export const deleteUser = (socketID) => {
+  return new Promise((resolve, reject) => {
+    const deleteQuery = "DELETE FROM users WHERE socketID = ?";
+    const params = [socketID];
+    db.run(deleteQuery, params, function (err) {
+      if (err) {
+        console.error("Error deleting user: ", err.message);
+        reject(err);
+      } else {
+        console.log("USER DELETED");
+        resolve();
+      }
+    });
   });
 };
